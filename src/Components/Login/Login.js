57a13363884e850e5e85785608c82b669d5d6775
Sakib Noman <skbnmn@gmail.com>
2021-03-19 05:27:49 +0600

    login signup made dynamic

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Login.css';
 
 const Login = () => {
+
+    const [option, setOption] = useState('login');
+
     return (
         <div className="d-flex flex-column justify-content-center align-items-center mt-5" >
             <div className="custom-form" >
                 <div className="container">
-                    <h4 className="mt-2" >Login</h4>
+                    <h4 className="mt-2" >{option === 'login' ? 'Login' : 'Create an account'}</h4>
+                    {option === 'signup' && <input type="text" class="input" placeholder="Name" />}
                     <input type="email" class="input" placeholder="Email" />
                     <input type="password" class="input" placeholder="Password" />
-                    <div className="d-flex justify-content-between align-items-baseline" >
+                    {option === 'signup' && <input type="password" class="input" placeholder="Confirm Password" />}
+                    {option === 'login' && <div className="d-flex justify-content-between align-items-baseline" >
                         <div>
                             <input type="checkbox" id="exampleCheck1" />
                             <label className="ml-2 mt-3" for="exampleCheck1">Remember me</label>
@@ -18,10 +23,12 @@ const Login = () => {
                         <Link>
                             <p className="text-danger" >Forgot Password</p>
                         </Link>
+                    </div>}
+                    <button type="button" className="btn btn-primary btn-block mt-3">{option === 'login' ? "Login" : "Create an account"}</button>
+                    <div className="d-flex justify-content-between align-items-baseline" >
+                        <p className="text-center mt-3" >{option === 'login' ? "Don't have an account?" : "Already have an account?"} </p>
+                        <Link onClick={() => option === 'login' ? setOption('signup') : setOption('login')} >{option === 'login' ? "Create an account" : "Login"}</Link>
                     </div>
-                    <button type="button" className="btn btn-primary btn-block">Login</button>
-                    <p className="text-center mt-3" >Don't have account? </p>
-                    <Link to="/login/signup" >Create an account</Link>
                 </div>
             </div>
             <p>or</p>
