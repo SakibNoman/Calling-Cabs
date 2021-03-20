2082a477137e03c1c5037a4f7c055998aeb4be06
Sakib Noman <skbnmn@gmail.com>
2021-03-20 12:08:24 +0600

    destination

--- src/Components/Destination/Destination.js
+++ src/Components/Destination/Destination.js
@@ -8,18 +8,20 @@ const Destination = () => {
         pickTo: '',
         time: ''
     })
+    const [isSearched, setIsSearched] = useState(false)
 
     const inputHandler = (e) => {
         const newLocation = { ...location };
         newLocation[e.target.id] = e.target.value;
         setLocation(newLocation);
     }
+    console.log(isSearched);
 
     return (
         <div className="row" >
             <div className="col-md-4">
                 <div className="container" >
-                    <form className=" Custom-cart container p-4 mt-5"  >
+                    {!isSearched && <form className=" Custom-cart container p-4 mt-5"  >
                         <div className="form-group">
                             <label htmlFor="exampleInputEmail1">Pick from</label>
                             <input onBlur={inputHandler} type="text" className="form-control" id="pickFrom" aria-describedby="emailHelp" />
@@ -33,11 +35,27 @@ const Destination = () => {
                                 <input onBlur={inputHandler} className="form-control" id="time" type="date" />
                             </div>
                         </div>
-                        <button type="button" className="btn btn-primary btn-block">Search</button>
-                    </form>
+                        <button onClick={() => isSearched ? setIsSearched(false) : setIsSearched(true)} type="button" className="btn btn-primary btn-block">Search</button>
+                    </form>}
+                    {
+                        isSearched && <form className=" Custom-cart container p-4 mt-5"  >
+                            <div className="routeName">
+                                <div className="timeline">
+                                    <div className="content1">
+                                        <h6>{location.pickFrom}</h6>
+                                    </div>
+                                    <div className="content2">
+                                        <h6>{location.pickTo}</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    }
                 </div>
             </div>
-            <div className="col-md-8"></div>
+            <div className="col-md-8">
+
+            </div>
         </div>
     );
 };
